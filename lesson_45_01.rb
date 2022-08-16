9b623b7b328f2bc5efea53a51fc34184cc22febe
Artem Lomakin <Lomakin-as@mail.ru>
2022-08-16 23:27:43 +0300

    Add lesson 45

---
+++ lesson_45_01.rb
@@ -0,0 +1,16 @@
+class Color 
+  def initialize
+  end
+
+  def red 
+    puts "Red"
+  end
+
+  def green
+    puts "Green"
+  end
+
+  def blue 
+    puts "Blue"
+  end
+end
---
+++ lesson_45_02.rb
@@ -0,0 +1,7 @@
+require "./lesson_45_01.rb"
+
+color = Color.new 
+
+color.red
+color.blue
+color.green

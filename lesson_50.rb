d03fc851d492e329832ae20f8da98962c339cc54
Artem Lomakin <Lomakin-as@mail.ru>
2022-08-18 16:29:10 +0300

    Add lesson 50

---
+++ lesson_50.rb
@@ -0,0 +1,19 @@
+hero = ["Бэтмен", "Фродо Бэггинс", "Шерлок Холмс"]
+villan = ["Джокер", "Профессор Мориарти", "Саурон"]
+
+puts "Enter hero person (Бэтмен, Фродо Бэггинс, Шерлок Холмс): "
+user_choose_hero = STDIN.gets.chomp.capitalize
+
+case user_choose_hero
+when "Бэтмен", "Batman"
+  puts villan[0]
+when "Фродо Бэггинс"
+  puts villan[2]
+when "Шерлок Холмс"
+  puts villan[1]
+else 
+  puts "Please, enter hero"
+end
+
+
+

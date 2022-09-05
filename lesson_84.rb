19c2159b8f59e441a11472f5431ab4f3c57f11e5
Artem Lomakin <Lomakin-as@mail.ru>
2022-09-05 19:11:59 +0300

    Add lesson 86

---
+++ lesson_84.rb
@@ -0,0 +1,5 @@
+values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
+# suits = ["Diamonts", "Hearts", "Clubs", "Spades"]
+suits = ["♠", "♥", "♦", "♣"]
+
+puts "Random carts: #{suits.sample}, #{values.sample}"
